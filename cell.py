662067748e6cfbe6fe3b76e04d41f1d4ab1a7e0f
12nathanb <nathan@thebarnetts.co.uk>
2020-11-22 15:20:42 +0000

    Add files via upload

--- cell.py
+++ cell.py
@@ -11,7 +11,7 @@ class Cell:
         self.state = state
         self.screen = screen
         self.size = size
-
+        self.age = 0
        
         
 
@@ -36,7 +36,55 @@ class Cell:
     def setColor(self):
         BLACK = ( 0, 0, 0)
         WHITE = ( 255, 255, 255)
+        GREEN1 = (255, 13, 0)
+        GREEN2 = (232, 12, 0)
+        GREEN3 = (217, 13, 2)
+        GREEN4 = (201, 10, 0)
+        GREEN5 = (181, 11, 2)
+        GREEN6 = (163, 10, 2)
+        GREEN7 = (145, 7, 0)
+        GREEN8 = (128, 6, 0)
+        GREEN9 = (110, 5, 0)
+        GREEN10 = (94, 4, 0)
+        
+        RED = (222, 28, 18)
+        
         if self.state == "DEAD":
-            self.color = WHITE
-        else:
+            self.age  = 0
             self.color = BLACK
+        else:
+            self.age  = self.age  + 1
+            
+        if self.state == "ALIVE":
+            if self.age == 1:
+                self.color = GREEN1
+
+            if self.age == 2:
+                self.color = GREEN2
+
+            if self.age == 3:
+                self.color = GREEN3
+            
+            if self.age == 4:
+                self.color = GREEN4
+
+            if self.age == 5:
+                self.color = GREEN5
+
+            if self.age == 6:
+                self.color = GREEN6
+
+            if self.age == 7:
+                self.color = GREEN7
+            
+            if self.age == 8:
+                self.color = GREEN8
+            
+            if self.age == 9:
+                self.color = GREEN9
+            
+            if self.age == 10:
+                self.color = GREEN10
+
+            if self.age > 10:
+                self.color = BLACK
--- main.py
+++ main.py
@@ -2,15 +2,85 @@ import pygame
 import random
 from cell import Cell
 
-def nearestNeigbour(c, x, y):
-    #x = int(c.getX())
-    #y = int(c.getY())
-    s = c.getState()
+def gameOfLife(amount, x, y):
+
+    if amount > 0:
+
+        if cellArray[x][y].getState() == "ALIVE" and amount < 2:
+            nextSetStateArray[x][y] = "DEAD"
+
+        elif cellArray[x][y].getState() == "ALIVE" and amount == 2:
+            nextSetStateArray[x][y] = "ALIVE"
+
+        elif cellArray[x][y].getState() == "ALIVE" and amount == 3:
+            nextSetStateArray[x][y] = "ALIVE"
+
+        elif cellArray[x][y].getState() == "ALIVE" and amount > 3:
+            nextSetStateArray[x][y] = "DEAD"
+
+        elif cellArray[x][y].getState() == "DEAD" and amount == 3:
+            nextSetStateArray[x][y] = "ALIVE"
+
+def oddOrEven(amount, x, y):
     
+    if amount % 2 == 0:
+        nextSetStateArray[x][y] = "ALIVE"
 
+    else:
+        nextSetStateArray[x][y] = "DEAD"
+
+def makeACircle():
+    cellArray[49][47].setState("ALIVE")
+    cellArray[50][47].setState("ALIVE")
+
+    cellArray[48][48].setState("ALIVE")
+    cellArray[49][48].setState("ALIVE")
+    cellArray[50][48].setState("ALIVE")
+    cellArray[51][48].setState("ALIVE")
+
+    cellArray[47][49].setState("ALIVE")
+    cellArray[48][49].setState("ALIVE")
+    cellArray[49][49].setState("ALIVE")
+    cellArray[50][49].setState("ALIVE")
+    cellArray[51][49].setState("ALIVE")
+    cellArray[52][49].setState("ALIVE")
+
+    cellArray[48][50].setState("ALIVE")
+    cellArray[49][50].setState("ALIVE")
+    cellArray[50][50].setState("ALIVE")
+    cellArray[51][50].setState("ALIVE")
+
+    cellArray[49][51].setState("ALIVE")
+    cellArray[50][52].setState("ALIVE")
+
+    for i in range(gridSize):
+        for j in range(gridSize):
+            nextSetStateArray[i][j] = cellArray[i][j].getState()
+
+def nearestNeigbourOf3(c, x, y):
+    counter = ""
     if x > 0 and x < gridSize - 1 and  y > 0 and y < gridSize - 1:
-        counter = 0
 
+        if cellArray[(x - 1)][(y)].getState() == "ALIVE": 
+            counter + "1"
+        else:
+            counter + "0"
+
+        if cellArray[(x)][(y)].getState() == "ALIVE": 
+            counter + "1"
+        else:
+            counter + "0"
+
+        if cellArray[(x + 1)][(y)].getState() == "ALIVE": 
+            counter + "1"
+        else:
+            counter + "0"
+    
+    return counter
+
+def nearestNeigbour(c, x, y):
+    counter = 0
+    if x > 0 and x < gridSize - 1 and  y > 0 and y < gridSize - 1:
         
 
         if cellArray[(x - 1)][(y - 1)].getState() == "ALIVE":
@@ -43,34 +113,54 @@ def nearestNeigbour(c, x, y):
 
         if cellArray[(x + 1)][(y + 1)].getState() == "ALIVE": 
             counter = counter + 1
-           
 
-        if cellArray[x][y].getState() == "ALIVE" and counter < 2:
-            nextSetStateArray[x][y] = "DEAD"
+    return counter
 
-        elif cellArray[x][y].getState() == "ALIVE" and counter == 2:
-            nextSetStateArray[x][y] = "ALIVE"
+def rule30(r, x, y):
+    if r == "111":
+        nextSetStateArray[x][y + 1] = "DEAD"
+    elif r == "110":
+        nextSetStateArray[x][y + 1] = "DEAD"
+        nextSetStateArray[x + 1][y] = "DEAD"
+    elif r == "101":
+        nextSetStateArray[x][y + 1] = "DEAD"
+        nextSetStateArray[x][y] = "DEAD"
+    elif r == "100":
+        nextSetStateArray[x + 1][y] = "DEAD"
+        nextSetStateArray[x][y] = "DEAD"
+    elif r == "011":
+        nextSetStateArray[x - 1][y] = "DEAD"
+    elif r == "010":
+        nextSetStateArray[x + 1][y] = "DEAD"
+        nextSetStateArray[x - 1][y] = "DEAD"
+    elif r == "001":
+        nextSetStateArray[x][y] = "DEAD"
+        nextSetStateArray[x - 1][y] = "DEAD"
+    elif r == "000":
+        nextSetStateArray[x + 1][y] = "DEAD"
+        nextSetStateArray[x - 1][y] = "DEAD"
+        nextSetStateArray[x][y] = "DEAD"
+        nextSetStateArray[x][y + y] = "DEAD"
 
-        elif cellArray[x][y].getState() == "ALIVE" and counter == 3:
-            nextSetStateArray[x][y] = "ALIVE"
 
-        elif cellArray[x][y].getState() == "ALIVE" and counter > 3:
-            nextSetStateArray[x][y] = "DEAD"
+        
+def setupCellArray():
+    for i in range(gridSize):
+        for j in range(gridSize):
+            t = random.randint(0, 1)
 
-        elif cellArray[x][y].getState() == "DEAD" and counter == 3:
-            nextSetStateArray[x][y] = "ALIVE"
-
-    
-    
-
+            cellArray[i][j] = Cell(j , i , cellSize, stateArray[t], screen)
+            
+            nextSetStateArray[i][j] = cellArray[i][j].getState()
+            cellArray[i][j].setColor()
 
 pygame.init()
 clock = pygame.time.Clock()
-screenSize = 500
-gridSize = 50
+screenSize = 1000
+gridSize = 100
 cellSize = screenSize / gridSize
 screen = pygame.display.set_mode((screenSize, screenSize))
-
+RUN = False
 BLACK = ( 0, 0, 0)
 WHITE = ( 255, 255, 255)
 GREEN = ( 0, 255, 0)
@@ -82,21 +172,12 @@ stateArray = ["ALIVE", "DEAD"]
 
 nextSetStateArray = [ [" "]* gridSize for i in range(gridSize)]
 
-for i in range(gridSize):
-    for j in range(gridSize):
-        ranInt = random.randint(0, 1)
-        cellArray[i][j] = Cell(j , i , cellSize, stateArray[ranInt], screen)
-        nextSetStateArray[i][j] = cellArray[i][j].getState()
-        cellArray[i][j].setColor()
-        #cellArray.append()
+setupCellArray()
+#makeACircle()
         
-
 running = True
 
 while running:
-
-
-    # Did the user click the window close button?
 
     for event in pygame.event.get():
 
@@ -105,41 +186,32 @@ while running:
             running = False
 
 
-    # Fill the background with white
-
     screen.fill(WHITE)
-    #test.draw()
-    #for x in cellArray:
-        
-       # x.draw()
-    
-    for i in range(gridSize):
-        for j in range(gridSize):
-           
-            cellArray[i][j].draw()
-            cellArray[i][j].setColor()
-            nearestNeigbour(cellArray[i][j], i, j)
-    
-    for i in range(gridSize):
-            for j in range(gridSize):
-                cellArray[i][j].setState(nextSetStateArray[i][j])
-    
-    if event.type == pygame.MOUSEBUTTONDOWN:
 
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            RUN = True
+            
+  
+    if RUN == True:
         for i in range(gridSize):
             for j in range(gridSize):
-                state1 = cellArray[i][j].getState()
-                nearestNeigbour(cellArray[i][j], i, j)
-                state2 = cellArray[i][j].getState()
-                print("STATE 1: {} STATE 2: {}".format(state1, state2))
-        
-        
+            
+                cellArray[i][j].draw()
+                cellArray[i][j].setColor()
+                #oddOrEven(nearestNeigbourOf3(cellArray[i][j], i, j), i, j)
+                rule30(nearestNeigbourOf3(cellArray[i][j], i, j), i, j)
 
-    # Flip the display
-
+        
+        for i in range(gridSize):
+                for j in range(gridSize):
+                    cellArray[i][j].setState(nextSetStateArray[i][j])
+                    
+    
+    
     pygame.display.flip()
 
-    clock.tick(2)
+    clock.tick(15)
 
 # Done! Time to quit.
 
